061299bedc0bf434fb5d67ef91760785bcc0c12b
Giuseppe Silvi <grammaton@me.com>
2019-05-02 11:02:21 +0200

    faustmacos.sh update

--- faustmacos.sh
+++ faustmacos.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # use it by terminal typing:
-# bash seampull.sh
+# bash faustmacos.sh
 
 say Will do, sir
 
@@ -13,11 +13,17 @@ mkdir GitHub
 
 cd GitHub
 
+echo "Intalling HomeBrew"
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Intalling cmake, pkgconfig, and qt"
 
 brew install cmake pkgconfig qt
 
 echo 'export PATH="/usr/local/opt/qt/bin:$PATH"' >> ~/.bash_profile
+
+echo "Cloning Faust repository into ~/Documents/GitHub"
 
 git clone https://github.com/grame-cncm/faust.git
 
@@ -25,8 +31,12 @@ cd faust
 
 git submodule update --init
 
+echo "Intalling Faust"
+
 make
 
 sudo make install
 
-say ciao belli
+say Congratulations! You have created a new element
+
+open .
